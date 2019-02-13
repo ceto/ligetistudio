ee721be941981da9aa5cc8ed70319ef7f9cb13dc
Szabó Gábor <szabogabi@gmail.com>
2019-02-13 15:59:25 +0100

    google analitycs added

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,17 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <?php if ( !current_user_can( 'manage_options' ) ) : ?>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-8789773-1"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-8789773-1');
+        </script>
+    <?php endif; ?>
 </head>
